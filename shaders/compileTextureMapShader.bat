4930ede69fb73a769b876b53ed8b73b22c285eac
Shivar Jadoonanan <sbshivar@gmail.com>
2025-05-01 17:00:44 -0400

    Delete shaders/compileTextureMapShader.bat

--- shaders/compileTextureMapShader.bat
+++
@@ -1,4 +0,0 @@
-del textureMapFrag.spv
-del textureMapVert.spv
-C:\VulkanSDK\1.4.304.1\Bin\glslc.exe textureMapVert.vert -o textureMapVert.spv
-C:\VulkanSDK\1.4.304.1\Bin\glslc.exe textureMapFrag.frag -o textureMapFrag.spv
